9f15d5175d69ed6b6bc73f7521c64460b210fbd4
philipwmitilinos-max <philipwmitilinos@gmail.com>
2025-09-09 10:44:41 +0200

    Testar. Läggde i år moled.

---
+++ src/main/java/se/iths/philip/project1/Car.java
@@ -0,0 +1,45 @@
+package se.iths.philip.project1;
+
+public class Car {
+    private String brand;
+    private String color;
+    private double price;
+    private int year;
+
+    public Car(String brand, String color, double price, int year) {
+        this.brand = brand;
+        this.color = color;
+        this.price = price;
+        this.year = year;
+
+    }
+
+    public Car() {
+
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void printInfo() {
+        System.out.println("Märke: " + brand);
+        System.out.println("Färg: " + color);
+        System.out.println("Pris: " + price);
+        System.out.println("Års model: " + year);
+
+    }
+}
+
---
+++ src/main/java/se/iths/philip/project1/project1Main.java
@@ -0,0 +1,9 @@
+package se.iths.philip.project1;
+
+public class project1Main {
+    public static void main(String[] args) {
+        Car volvo = new Car("Volvo", "Röd", 10000, 2015);
+
+        volvo.printInfo();
+    }
+}
